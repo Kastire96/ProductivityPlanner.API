2e9dcca53b366dd72594ef82d282c8a3d6274558
Kastire96 <kamilkemp@wp.pl>
2026-05-11 20:46:33 +0200

    feat: utworzenie projektu ProductivityPlanner.Mobile w technologii .NET MAUI

---
+++ ProductivityPlanner.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductivityPlanner.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
